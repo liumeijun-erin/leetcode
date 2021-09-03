493f5deb644afb9febcab6b0ec2b5c5b76efce23
emoji16 <tracy_2921@yeah.net>
2021-09-03 10:11:31 +0800

    uf

--- code/684.冗余连接.cpp
+++ code/684.冗余连接.cpp
@@ -1,17 +1,28 @@
 class UF{
-    vector<int> id;
+    vector<int> id,size;
 public:
-    UF(int n):id(n){
+    UF(int n):id(n),size(n,1){
         iota(id.begin(),id.end(),0);
     }
     int find(int p){
         while(id[p]!=p){
+            id[p] = id[id[p]];
             p = id[p];
         }
         return p;
     }
     void connect(int p,int q){
-        id[find(p)] = find(q);
+        int i = find(p),j = find(q);
+        if(i!=j){
+            if(size[i] < size[j]){
+                id[i] = j;
+                size[j] += size[i];
+            }
+            else{
+                id[j] = i;
+                size[i] += size[j];
+            }
+        }
     }
     bool isConnected(int p,int q){
         return find(p) == find(q);
