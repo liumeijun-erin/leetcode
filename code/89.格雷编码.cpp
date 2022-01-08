9f485a9b9d5f089712d62704cbc44b6b96b56283
emoji16 <tracy_2921@yeah.net>
2022-01-08 12:11:45 +0800

    按位计算

--- code/89.格雷编码.cpp
+++ code/89.格雷编码.cpp
@@ -8,18 +8,15 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        vector<int> res {0};
-        int cnt = 1;
-        if(!n) return res;
+        vector<int> res = {0};
+        int cnt = 1;;
         while(n--){
-            int len = res.size();
-            for(int i = len-1;i >= 0;--i){
+            for(int i = res.size()-1;i >= 0;--i){
                 res.emplace_back(cnt+res[i]);
             }
             cnt *= 2;
         }
         return res;
-        //cnt *= 2;
     }
 };
 // @lc code=end
