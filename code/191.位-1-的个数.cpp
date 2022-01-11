23c4538622396f07d887d570be51b82462761f7d
emoji16 <tracy_2921@yeah.net>
2022-01-11 15:02:32 +0800

    n&(n-1)降低最后一位

--- code/191.位-1-的个数.cpp
+++ code/191.位-1-的个数.cpp
@@ -8,10 +8,20 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
+        // solution1:标准遍历，O(len)
+        // int cnt = 0;
+        // while(n){
+        //     if(n%2) ++cnt;
+        //     n /= 2;
+        // }
+        // return cnt;
+        
+        // solution2：n&(n-1)将n中最优一位的1变成n的结果O(len_of_1)
+        // n &= (n-1)作用！
         int cnt = 0;
-        while(n){
-            if(n%2) ++cnt;
-            n /= 2;
+        while (n) {
+            n &= (n-1);
+            ++cnt;
         }
         return cnt;
     }
