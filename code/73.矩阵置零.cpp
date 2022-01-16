3a4d30c257ca176e9c7238c047944d87f65b3360
emoji16 <tracy_2921@yeah.net>
2022-01-16 20:04:51 +0800

    利用已有空间进行标记; int ij 只定义一次快一点

--- code/73.矩阵置零.cpp
+++ code/73.矩阵置零.cpp
@@ -28,41 +28,32 @@ public:
             }
         }*/
         //solution2:使用第一行作为列标记（利用有零全零则用非零标记，无零则用零标记）;而行随第一次遍历即时更改
-        int m =matrix.size(),n = matrix[0].size();
-        int flag  = 0,i,j;
-        for(j = 0;j < n;++j){
-            if(!matrix[0][j]) {
-                flag = 1;
-                break;
-            }
-        }
-        if(flag){
-            for(j = 0;j < n;++j) {
-                if(!matrix[0][j]) matrix[0][j] = flag;
-                else matrix[0][j] = 0;
-            }
-        }
-        for(i = 1;i < m;++i){
-            bool hasZero = false;
-            for(j = 0;j < n;++j){
-                if(!matrix[i][j])  {
-                    hasZero = true;
-                    matrix[0][j] = flag;
-                }
-            }
-            if(hasZero){
-                for(j = 0;j < n;++j){
-                    matrix[i][j] = 0;
-                }   
-            }
-        }
-        for(j = 0;j < n;++j){
-            if(matrix[0][j] == flag){
-                for(i = 0;i < m;++i){
-                    matrix[i][j] = 0;
+        // i,j 常用的话定义一次性能好
+        int m = matrix.size(), n = matrix[0].size();
+        int i, j; 
+        bool row_0 = false, col_0 = false;
+        for (i = 0; i < m; ++i) {
+            for (j = 0; j < n; ++j) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                    if (i == 0) row_0 = true;
+                    if (j == 0) col_0 = true;
                 }
             }
         }
+        for (i = 1; i < m; ++i) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < n; ++j)  matrix[i][j] = 0;
+            }
+        }
+        for (j = 1; j < n; ++j) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < m; ++i)  matrix[i][j] = 0;
+            }
+        }
+        if (col_0) for (i = 0; i < m; ++i)  matrix[i][0] = 0;
+        if (row_0) for (j = 0; j < n; ++j)  matrix[0][j] = 0;
     }
 };
 // @lc code=end
