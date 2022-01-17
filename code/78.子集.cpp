68e18ad158e07661e7027e6a49a3fd28f351724b
emoji16 <tracy_2921@yeah.net>
2022-01-17 11:00:59 +0800

    遍历

--- code/78.子集.cpp
+++ code/78.子集.cpp
@@ -7,18 +7,17 @@
 // @lc code=start
 class Solution {
 public:
-    void subsets(vector<vector<int> >& res,vector<int>& nums,int len){
-        if(len != 0) subsets(res,nums,len-1);
-        int size = res.size();
-        for(int i =0;i < size;++i){
-            vector<int> tmp = res[i];
-            tmp.emplace_back(nums[len]);
-            res.emplace_back(tmp);
-        }
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int> > res{vector<int> {}};
-        subsets(res,nums,nums.size()-1); 
+        vector<vector<int> > res = {vector<int> {}};
+        for (const int& num : nums) {
+            // cout<<num<<endl;
+            vector<vector<int> > tmp(res);
+            for (vector<int>& v: tmp) {
+                vector<int> tmp_v(v);
+                tmp_v.emplace_back(num);
+                res.emplace_back(tmp_v);
+            }
+        }
         return res;
     }
 };
