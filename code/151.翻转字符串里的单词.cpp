f0ca96b51f73f034e1019333a4e43627047171dd
emoji16 <tracy_2921@yeah.net>
2022-03-11 14:12:19 +0800

    优化到只用一阶空间

--- code/151.翻转字符串里的单词.cpp
+++ code/151.翻转字符串里的单词.cpp
@@ -7,24 +7,61 @@
 // @lc code=start
 class Solution {
 public:
-    // note:参考答案，不用两边分别识别word加入left_s, right_s
-    // 从头开始往后面塞即可
-    // note2:可以对新string进行操作 而不用s.erase
+    class Solution {
+public:
     string reverseWords(string s) {
-        string res = "";
-        int pos = s.size()-1;
-        while (pos >= 0) {
-            while (pos >= 0 && s[pos] == ' ') --pos;
-            if (pos < 0) break;
-            int pre = pos;
-            while(pre >= 0 && s[pre] != ' ') --pre;
-            res += (s.substr(pre+1, pos-pre) + ' ');
-            pos = pre;
+        int left = 0, right = s.size()-1;
+        while (left < right) {
+            while (left <= right && s[left] == ' ') ++left;
+            while (left <= right && s[right] == ' ') --right;
+            int left_tmp = left;
+            while (left <= right && s[left] != ' ') ++left;
+            if (left >= right) break;
+            int right_tmp = right;
+            while (left <= right && s[right] != ' ') --right;
+            
+            // left_tmp left-1 right+1 right_tmp
+            string tmp_left = s.substr(left_tmp, left-left_tmp);
+            string tmp_right = s.substr(right+1, right_tmp-right);
+            // cout<<tmp_left<<" "<<tmp_right<<endl;
+            int gap = left_tmp + tmp_right.size() - left;
+            // cout<<gap<<endl;
+            if (gap > 0) {
+                for (int i = right + gap;i-gap >= left;--i) {
+                    s[i] = s[i - gap];
+                }
+                left += gap;
+                right += gap;
+            }
+            else if (gap < 0){
+                for (int i = left-1+gap; i <= right-1+gap; ++i) {
+                    s[i] = s[i-gap];
+                }
+                left += gap;
+                right += gap;
+            }
+            for (const char& c: tmp_right) {
+                s[left_tmp++] = c;
+            }
+            s[left_tmp++] = ' ';
+            for (int i = tmp_left.size()-1; i>=0; --i) {
+                s[right_tmp--] = tmp_left[i];
+            }
+            s[right_tmp--] = ' ';
+            // cout<<s<<endl;
         }
-        if (res.size()) res.pop_back();
-        return res;
+        left = 0;
+        // cout<<s<<endl;
+        for (int pos = 0; pos < s.size(); ++pos) {
+            if (s[pos] ==' ') {
+                if (left != 0 && s[left-1] != ' ') s[left++] = ' ';
+            }
+            else s[left++] = s[pos];
+        }
+        if (s[left-1] == ' ') --left;
+        return s.substr(0, left);
     }
-    //"  hello world  "
+};
 };
 // @lc code=end
 
