b6b6ce1c064d4a21e19a1a6414cafe12893e83c9
emoji16 <tracy_2921@yeah.net>
2022-03-20 10:49:45 +0800

    优化拓扑排序代码; 使用queue结构记录所有可能发生变化的点，这样可以避免n^2搜索indegree == 0

--- code/210.课程表-ii.cpp
+++ code/210.课程表-ii.cpp
@@ -8,9 +8,10 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        //参考了答案：利用indegree数组！！！
-        //思考极限情况：即不存在合理解的情况是怎样的--相互依赖！！
-        //todo：也可以拍成dfs+visit
+        // 参考了答案：利用indegree数组！！！
+        // 思考极限情况：即不存在合理解的情况是怎样的--相互依赖！！
+        // 参考答案：避免不断搜索入度为0的节点；
+        // trick:使用queue记录所有可能发生变化的点!!!
         vector<int> indegree(numCourses);//pre
         vector<vector<int> > next(numCourses);
         for(const vector<int>& edge:prerequisites){
@@ -18,20 +19,32 @@ public:
             next[edge[1]].emplace_back(edge[0]);
         }
         vector<int> res;
-        while(1){
-            bool done = true;
-            for(int i = 0;i < numCourses;++i){
-                if(!indegree[i]){
-                    done = false;
-                    res.emplace_back(i);
-                    indegree[i] = -1;
-                    for(const int& n:next[i]){
-                        --indegree[n];
-                    }
-                }
-            }
-            if(done) break;
+        queue<int> q;
+        for (int i = 0; i < numCourses; ++i) {
+            if (!indegree[i]) q.emplace(i);
         }
+        while (!q.empty()) {
+            res.emplace_back(q.front());
+            for (const int& to: edge[q.front()]) {
+                --indegree[to];
+                if (indegree[to] == 0) q.emplace(to);
+            }
+            q.pop();
+        }
+        // while(1){
+        //     bool done = true;
+        //     for(int i = 0;i < numCourses;++i){
+        //         if(!indegree[i]){
+        //             done = false;
+        //             res.emplace_back(i);
+        //             indegree[i] = -1;
+        //             for(const int& n:next[i]){
+        //                 --indegree[n];
+        //             }
+        //         }
+        //     }
+        //     if(done) break;
+        // }
         if(res.size() != numCourses) res.clear();
         return res;
     }
